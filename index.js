eb8e727e353532f6f0b7611d5b32a875cb3a9a70
Andreas Hippler <Andreas.Hippler@logmein.com>
2020-11-26 12:51:02 +0100

    chore: replace execa with exec, fix linting

--- index.js
+++ index.js
@@ -1,76 +1,130 @@
-'use strict';
-const execa = require('execa');
+"use strict";
 
-const macos = () => execa.stdout('netstat', ['-anv', '-p', 'tcp'])
-	.then(data => Promise.all([data, execa.stdout('netstat', ['-anv', '-p', 'udp'])]))
-	.then(data => data.join('\n'));
+const childProcess = require("child_process");
+const { promisify } = require("util");
 
-const linux = () => execa.stdout('ss', ['-tunlp']);
-const win32 = () => execa.stdout('netstat', ['-ano']);
+const exec = promisify(childProcess.exec);
 
-const getListFn = process.platform === 'darwin' ? macos : process.platform === 'linux' ? linux : win32;
-const cols = process.platform === 'darwin' ? [3, 8] : process.platform === 'linux' ? [4, 6] : [1, 4];
-const isProtocol = x => /^\s*(tcp|udp)/i.test(x);
+async function execa(command) {
+    const { stdout } = await exec(command);
+    return stdout.trim();
+}
 
-const parsePid = input => {
-	if (typeof input !== 'string') {
-		return null;
-	}
+function colsForPlatform() {
+    switch (process.platform) {
+        case "darwin":
+            return [3, 8];
+        case "linux":
+            return [4, 6];
+        case "win32":
+            return [1, 4];
+        default:
+            throw new Error(`Unsupported platform \`${process.platform}\``);
+    }
+}
 
-	const match = input.match(/(?:^|",|",pid=)(\d+)/);
-	return match ? parseInt(match[1], 10) : null;
+const cols = colsForPlatform();
+
+async function darwin() {
+    const protocols = await Promise.all([
+        execa("netstat -anv -p tcp"),
+        execa("netstat -anv -p udp"),
+    ]);
+    return protocols.join("\n");
+}
+
+async function linux() {
+    return execa("ss -tunlp");
+}
+
+async function win32() {
+    return execa("netstat -ano");
+}
+
+function isProtocol(x) {
+    return /^\s*(tcp|udp)/i.test(x);
+}
+
+function parsePid(input) {
+    if (typeof input !== "string") {
+        return null;
+    }
+
+    const match = input.match(/(?:^|",|",pid=)(\d+)/);
+    return match ? Number.parseInt(match[1], 10) : null;
+}
+
+function getPort(input, list) {
+    const regex = new RegExp(`[.:]${input}$`);
+    const port = list.find((x) => regex.test(x[cols[0]]));
+
+    if (!port) {
+        throw new Error(`Couldn't find a process with port \`${input}\``);
+    }
+
+    return parsePid(port[cols[1]]);
+}
+
+async function getListForPlatform() {
+    switch (process.platform) {
+        case "darwin":
+            return darwin();
+        case "linux":
+            return linux();
+        case "win32":
+            return win32();
+        default:
+            throw new Error(`Unsupported platform \`${process.platform}\``);
+    }
+}
+
+async function getList() {
+    const result = [];
+    const list = await getListForPlatform();
+    const lines = list.split("\n");
+    lines.forEach((line) => {
+        if (isProtocol(line)) {
+            result.push(line.match(/\S+/g) || []);
+        }
+    });
+    return result;
+}
+
+module.exports = async (input) => {
+    if (typeof input !== "number") {
+        return Promise.reject(
+            new TypeError(`Expected a number, got ${typeof input}`),
+        );
+    }
+
+    const list = await getList();
+    return getPort(input, list);
 };
 
-const getPort = (input, list) => {
-	const regex = new RegExp(`[.:]${input}$`);
-	const port = list.find(x => regex.test(x[cols[0]]));
+module.exports.all = async (input) => {
+    if (!Array.isArray(input)) {
+        return Promise.reject(
+            new TypeError(`Expected an array, got ${typeof input}`),
+        );
+    }
 
-	if (!port) {
-		throw new Error(`Couldn't find a process with port \`${input}\``);
-	}
-
-	return parsePid(port[cols[1]]);
+    const list = await getList();
+    const array = await Promise.all(input.map((x) => [x, getPort(x, list)]));
+    return new Map(array);
 };
 
-const getList = () => getListFn().then(list => list
-	.split('\n')
-	.reduce((result, x) => {
-		if (isProtocol(x)) {
-			result.push(x.match(/\S+/g) || []);
-		}
+module.exports.list = async () => {
+    const list = await getList();
+    const map = new Map();
 
-		return result;
-	}, [])
-);
+    list.forEach((x) => {
+        const match = x[cols[0]].match(/[^]*[.:](\d+)$/);
+        if (match) {
+            const port = Number.parseInt(match[1], 10);
+            const pid = parsePid(x[cols[1]]);
+            map.set(port, pid);
+        }
+    });
 
-module.exports = input => {
-	if (typeof input !== 'number') {
-		return Promise.reject(new TypeError(`Expected a number, got ${typeof input}`));
-	}
-
-	return getList().then(list => getPort(input, list));
+    return map;
 };
-
-module.exports.all = input => {
-	if (!Array.isArray(input)) {
-		return Promise.reject(new TypeError(`Expected an array, got ${typeof input}`));
-	}
-
-	return getList()
-		.then(list => Promise.all(input.map(x => [x, getPort(x, list)])))
-		.then(list => new Map(list));
-};
-
-module.exports.list = () => getList().then(list => {
-	const ret = new Map();
-
-	for (const x of list) {
-		const match = x[cols[0]].match(/[^]*[.:](\d+)$/);
-
-		if (match) {
-			ret.set(parseInt(match[1], 10), parsePid(x[cols[1]]));
-		}
-	}
-
-	return ret;
-});
--- test.js
+++ test.js
@@ -1,45 +1,58 @@
-import http from 'http';
-import {serial as test} from 'ava';
-import getPort from 'get-port';
-import m from '.';
+const http = require("http");
+const test = require("ava");
+const getPort = require("get-port");
+const pidFromPort = require(".");
 
-const srv = () => http.createServer((req, res) => {
-	res.end();
+const srv = () =>
+    http.createServer((_, response) => {
+        response.end();
+    });
+
+test.serial("success", async (t) => {
+    const port = await getPort();
+    const server = srv().listen(port);
+    t.truthy(await pidFromPort(port));
+    server.close();
 });
 
-test('success', async t => {
-	const port = await getPort();
-	const server = srv().listen(port);
-	t.truthy(await m(port));
-	server.close();
+test.serial("fail", async (t) => {
+    await t.throwsAsync(
+        () => pidFromPort(0),
+        null,
+        "Couldn't find a process with port `0`",
+    );
+    await t.throwsAsync(
+        () => pidFromPort.all([0]),
+        null,
+        "Couldn't find a process with port `0`",
+    );
 });
 
-test('fail', async t => {
-	await t.throws(m(0), 'Couldn\'t find a process with port `0`');
-	await t.throws(m.all([0]), 'Couldn\'t find a process with port `0`');
+test.serial("accepts a number", async (t) => {
+    await t.throwsAsync(
+        () => pidFromPort("foo"),
+        null,
+        "Expected a number, got string",
+    );
 });
 
-test('accepts a number', async t => {
-	await t.throws(m('foo'), 'Expected a number, got string');
+test.serial("all", async (t) => {
+    const [p1, p2] = await Promise.all([getPort(), getPort()]);
+    const [s1, s2] = [srv().listen(p1), srv().listen(p2)];
+    const ports = await pidFromPort.all([p1, p2]);
+
+    t.true(ports instanceof Map);
+
+    for (const x of ports.values()) {
+        t.is(typeof x, "number");
+    }
+
+    s1.close();
+    s2.close();
 });
 
-test('all', async t => {
-	const [p1, p2] = await Promise.all([getPort(), getPort()]);
-	const [s1, s2] = [srv().listen(p1), srv().listen(p2)];
-	const ports = await m.all([p1, p2]);
-
-	t.true(ports instanceof Map);
-
-	for (const x of ports.values()) {
-		t.is(typeof x, 'number');
-	}
-
-	s1.close();
-	s2.close();
-});
-
-test('list', async t => {
-	const list = await m.list();
-	t.true(list instanceof Map);
-	await t.notThrows(m.all(Array.from(list.keys())));
+test.serial("list", async (t) => {
+    const list = await pidFromPort.list();
+    t.true(list instanceof Map);
+    await t.notThrowsAsync(() => pidFromPort.all([...list.keys()]));
 });
